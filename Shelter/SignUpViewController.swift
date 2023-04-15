adf55c47f855244f0d4db0971507aca80eccbb72
1219664875 <jesmanijismi@gmail.com>
2023-04-15 15:51:51 -0700

    Add a new text field incode in SignUpViewController

--- Shelter/SignUpViewController.swift
+++ Shelter/SignUpViewController.swift
@@ -13,27 +13,33 @@ class SignUpViewController: UIViewController, UITextFieldDelegate, UIGestureReco
     @IBOutlet var pNumberTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var cPasswordTextField: UITextField!
+    @IBOutlet var usernameTextField: UITextField!
+    
     @IBOutlet var haveAccountButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
     
     @IBOutlet var pNumberTextFieldContainer: UIView!
     @IBOutlet var passwordTextFieldContainer: UIView!
     @IBOutlet var cPasswordTextFieldContainer: UIView!
+    @IBOutlet var usernameTextFieldContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Set the UITextField delegate
+        usernameTextField.delegate = self
         pNumberTextField.delegate = self
         passwordTextField.delegate = self
         cPasswordTextField.delegate = self
         
+        usernameTextField.backgroundColor = .clear
         pNumberTextField.backgroundColor = .clear
         passwordTextField.backgroundColor = .clear
         cPasswordTextField.backgroundColor = .clear
         
         let cornerRadius: CGFloat = 10.0 // Change this value to adjust the corner radius
         
+        usernameTextFieldContainer.addShadow(cornerRadius: cornerRadius)
         pNumberTextFieldContainer.addShadow(cornerRadius: cornerRadius)
         passwordTextFieldContainer.addShadow(cornerRadius: cornerRadius)
         cPasswordTextFieldContainer.addShadow(cornerRadius: cornerRadius)
@@ -46,10 +52,14 @@ class SignUpViewController: UIViewController, UITextFieldDelegate, UIGestureReco
             textField.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: attributes)
         }
         
+        setThickerPlaceholder(for: usernameTextField, placeholderText: "Username")
         setThickerPlaceholder(for: pNumberTextField, placeholderText: "Phone Number")
         setThickerPlaceholder(for: passwordTextField, placeholderText: "Password")
         setThickerPlaceholder(for: cPasswordTextField, placeholderText: "Confirm Password")
-
+        
+        usernameTextField.layer.borderColor = UIColor.black.cgColor
+        usernameTextField.layer.borderWidth = 1.0
+        
         pNumberTextField.layer.borderColor = UIColor.black.cgColor
         pNumberTextField.layer.borderWidth = 1.0
 
